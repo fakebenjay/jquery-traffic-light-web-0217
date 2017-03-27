f106d0c9c69089eeab0a0bede50eae84d1bbcce2
fakebenjay <fakebenjay@gmail.com>
2017-03-27 16:17:59 -0400

    Done.

--- script.js
+++ script.js
@@ -1,28 +1,113 @@
+var x;
+var a;
+
+function changeColors() {
+    x = 1;
+    setInterval(change, 500);
+}
+
+function change() {
+    if (x === 1) {
+        color = "green";
+        x = 2;
+    }
+    else {
+        color = "";
+        x = 1;
+    }
+
+    if (a === 'stop') {
+      return
+    }
+
+    $('#bottom-light').css("background", color);
+}
 
 $(document).ready(function() {
-  
+
    /* code for button 1  -- Turn the light Red */
    $('#button1').click(
      function() {
-         $('#top-light').css("background", "red");
+       a = 'stop'
+       $('#top-light').css("background", "red");
+       $('#middle-light').css("background", "");
+       $('#bottom-light').css("background", "");
+       $('#middle-light').html("");
+       $('#bottom-light').html("");
      }
    );
+
    /* code for button 2 --- Say STOP */
    $('#button2').click(
      function() {
-         $('#top-light').html("<p> STOP </p>");
+       a = 'stop'
+       $('#top-light').html("<p> STOP </p>");
+       $('#middle-light').css("background", "");
+       $('#bottom-light').css("background", "");
+       $('#middle-light').html("");
+       $('#bottom-light').html("");
      }
    );
-  
-   /* code for button 3 -- Turn the light Yellow */
-  
-   /* code for button 4 -- Turn the light Green */
-  
-   /* code for button 5 -- Say GO! */
 
-   /* code for button 6 -- Make Green Light Blink -- Extension!! */
-  
+   /* code for button 3 -- Turn the light Yellow */
+   $('#button3').click(
+     function() {
+       a = 'stop'
+       $('#middle-light').css("background", "yellow");
+       $('#top-light').css("background", "");
+       $('#bottom-light').css("background", "");
+       $('#top-light').html("");
+       $('#bottom-light').html("");
+     }
+   );
+
+   /* code for button 4 -- Say whoa hey now */
+   $('#button4').click(
+     function() {
+       a = 'stop'
+       $('#middle-light').html("<p> whoa hey now </p>");
+       $('#top-light').css("background", "");
+       $('#bottom-light').css("background", "");
+       $('#top-light').html("");
+       $('#bottom-light').html("");
+     }
+   );
+
+   /* code for button 5 -- Turn the light Green */
+   $('#button5').click(
+     function() {
+       a = 'stop'
+       $('#bottom-light').css("background", "green");
+       $('#top-light').css("background", "");
+       $('#middle-light').css("background", "");
+       $('#top-light').html("");
+       $('#middle-light').html("");
+     }
+   );
+
+   /* code for button 6 -- Say GO! */
+   $('#button6').click(
+     function() {
+       $('#bottom-light').html("<p> GO! </p>");
+       $('#top-light').css("background", "");
+       $('#middle-light').css("background", "");
+       $('#top-light').html("");
+       $('#middle-light').html("");
+     }
+   );
+
+   /* code for button 7 -- Make Green Light Blink -- Extension!! */
+   $('#button7').click(
+     function() {
+       a = 'stop'
+       $('#bottom-light').css("background", "");
+       $('#top-light').css("background", "");
+       $('#middle-light').css("background", "");
+       $('#top-light').html("");
+       $('#middle-light').html("");
+       a = ''
+       changeColors();
+     }
+   );
+
 });
-                  
-                  
-                  
